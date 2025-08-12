c6623d34ff7b929b2ab6440ba9050c4c7f4fdc62
Edward Ojeda <edward.ojeda.es@gmail.com>
2025-08-11 20:38:53 -0500

    Feat: Add routing

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,7 @@
-import express from "express";
-
-const app = express();
-
-//Routing
-app.get('/', (req, res) => {
-  res.send('Hola mundo desde express con ts')
-})
-
-app.get('/home', (req, res) => {
-  res.send('HOlad desde HOME')
-})
+import server from './server.ts'
 
 const PORT = process.env.PORT || 4000
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 })
