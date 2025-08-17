b707150ae80751b35f3ac7a036a36d5deb3c82bf
Edward Ojeda <edward.ojeda.es@gmail.com>
2025-08-17 12:43:51 -0500

    Feat: Configuration react-router-dom

---
+++ frontend/src/router.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { LoginView } from './views/LoginView';
+
+export default function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route>
+          <Route path="/auth/login" element={<LoginView />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
---
+++ frontend/src/views/LoginView.tsx
@@ -0,0 +1,7 @@
+export const LoginView = () => {
+  return (
+    <div>
+      <h1>Login view</h1>
+    </div>
+  )
+}
