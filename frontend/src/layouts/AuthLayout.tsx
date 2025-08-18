373bbab477f29cd9833d13395fa267a1995d88e4
Edward Ojeda <edward.ojeda.es@gmail.com>
2025-08-18 16:50:01 -0500

    Feat: Creating layout for auth

---
+++ frontend/src/layouts/AuthLayout.tsx
@@ -0,0 +1,24 @@
+import { Outlet } from 'react-router-dom'
+import { Toaster } from 'sonner'
+
+export const AuthLayout = () => {
+  return (
+    <main className='relative min-h-screen bg-gray-50 flex items-center justify-center p-4'>
+      {/* Background decoration */}
+      <div className="absolute inset-0 w-full h-full bg-gradient-to-br from-violet-100 via-pink-100 to-orange-100 opacity-30"></div>
+
+      {/* Content Card */}
+      <div className='relative z-10 w-full max-w-md p-8 space-y-6 bg-white/70 backdrop-blur-xl rounded-2xl shadow-2xl'>
+        <div className='flex justify-center'>
+          <img src='/logo.svg' alt='Logotipo linkbio' className='w-64' />
+        </div>
+
+        <div>
+          <Outlet />
+        </div>
+      </div>
+
+      <Toaster position='top-center' richColors />
+    </main>
+  )
+}
