dcd4f1e0e1f367d43c7a1aa83c49fc4202ae19a2
Edward Ojeda <edward.ojeda.es@gmail.com>
2025-08-18 16:45:33 -0500

    Feat: Install config cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 import 'dotenv/config';
+import cors from 'cors';
 import router from './router';
 import { connectDB } from './config/db';
+import { corsConfig } from './config/cors';
 
 const app = express();
+
+app.use(cors(corsConfig))
 
 connectDB();
 
