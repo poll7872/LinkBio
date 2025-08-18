69a769233fd57a007ffb03866efa3fd0f2f11428
Edward Ojeda <edward.ojeda.es@gmail.com>
2025-08-18 16:49:43 -0500

    Feat: add types

---
+++ frontend/src/types/index.ts
@@ -0,0 +1,15 @@
+export type User = {
+  handle: string
+  name: string
+  email: string
+}
+
+export type RegisterForm = Pick<User, 'name' | 'email' | 'handle'> & {
+  password: string
+  password_confirmation: string
+}
+
+export type LoginForm = Pick<User, 'email'> & {
+  password: string
+}
+
