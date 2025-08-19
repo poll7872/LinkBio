96f6014c423bcb80c96dc7817c0c0b047d778d92
Edward Ojeda <edward.ojeda.es@gmail.com>
2025-08-19 17:24:27 -0500

    Feat: config endpoint getUser

---
+++ frontend/src/api/LinkBioAPI.ts
@@ -0,0 +1,14 @@
+import { isAxiosError } from "axios"
+import api from "../config/axios"
+
+export async function getUser() {
+  try {
+    const { data } = await api.get('/user')
+
+    return data
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error)
+    }
+  }
+}
