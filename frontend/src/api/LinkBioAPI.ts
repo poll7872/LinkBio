c9c441f168a519103d2c252e0a50ecbaa9f1d832
Edward Ojeda <edward.ojeda.es@gmail.com>
2025-08-21 19:45:48 -0500

    Feat: Consuming endpoint for get User and updateProfile

--- frontend/src/api/LinkBioAPI.ts
+++ frontend/src/api/LinkBioAPI.ts
@@ -1,9 +1,22 @@
 import { isAxiosError } from "axios"
 import api from "../config/axios"
+import type { profileForm, User } from "../types"
 
 export async function getUser() {
   try {
-    const { data } = await api.get('/user')
+    const { data } = await api.get<User>('/user')
+
+    return data
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error)
+    }
+  }
+}
+
+export async function updateProfile(formData: profileForm) {
+  try {
+    const { data } = await api.put('/user', formData)
 
     return data
   } catch (error) {
