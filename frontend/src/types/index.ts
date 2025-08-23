f569bf1b78cc0b4f5ca7ffe3a00aca4a7cc0ecd4
Edward Ojeda <edward.ojeda.es@gmail.com>
2025-08-23 16:28:10 -0500

    feat(frontend): add new types for social network and link tree

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -4,6 +4,7 @@ export type User = {
   email: string
   _id: string
   description: string
+  url_image: string
 }
 
 export type RegisterForm = Pick<User, 'name' | 'email' | 'handle'> & {
@@ -15,4 +16,13 @@ export type LoginForm = Pick<User, 'email'> & {
   password: string
 }
 
-export type profileForm = Pick<User, 'handle' | 'description'> 
+export type profileForm = Pick<User, 'handle' | 'description'>
+
+export type SocialNetwork = {
+  id: number
+  name: string
+  url: string
+  enabled: boolean
+}
+
+export type LinkTree = Pick<SocialNetwork, 'name' | 'url' | 'enabled'> 
