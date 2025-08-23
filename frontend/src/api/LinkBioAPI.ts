4cf710e97f347505096ade1b63e10864ff70209c
Edward Ojeda <edward.ojeda.es@gmail.com>
2025-08-23 16:28:28 -0500

    feat(frontend): update api calls for image upload and profile update

--- frontend/src/api/LinkBioAPI.ts
+++ frontend/src/api/LinkBioAPI.ts
@@ -14,10 +14,23 @@ export async function getUser() {
   }
 }
 
-export async function updateProfile(formData: profileForm) {
+export async function updateProfile(formData: User) {
   try {
     const { data } = await api.put('/user', formData)
+    return data
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error)
+    }
+  }
+}
 
+export async function uploadImage(file: File) {
+  let formData = new FormData()
+  formData.append('file', file)
+
+  try {
+    const { data } = await api.post('/user/image', formData)
     return data
   } catch (error) {
     if (isAxiosError(error) && error.response) {
